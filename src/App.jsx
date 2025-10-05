d7990e6595ff1504952381f35bbf38cc1f1dd5cb
Zebediah Allton <zebediah.allton@gmail.com>
2025-10-05 15:59:22 -0500

    Completed all required objectives.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
+import { tvShows } from "./shows/data";
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState(null);
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection
+          showData={tvShows}
+          selectedShow={selectedShow}
+          setSelectedShow={setSelectedShow}
+        ></ShowSelection>
       </header>
-      <main></main>
+      <main>
+        <ShowDetails
+          showData={tvShows}
+          selectedShow={selectedShow}
+          setSelectedShow={setSelectedShow}
+        ></ShowDetails>
+      </main>
     </>
   );
 }
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 import "./shows.css";
-
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ showData, selectedShow }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+  if (!selectedShow === true) {
+    return <p className="show">Please select a show to learn more.</p>;
+  }
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={selectedShow.name}
+        episodes={selectedShow.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      ></EpisodeList>
+      <EpisodeDetails episode={selectedEpisode}></EpisodeDetails>
+    </div>
+  );
 }
